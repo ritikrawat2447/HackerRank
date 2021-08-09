1c98b8a322eb0005aa71dc289b89703ac5a5758c
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2021-08-09 12:36:35 +0530

    Add files via upload

---
+++ Algorithms/Search/Ice_Cream_Parlor.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'icecreamParlor' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER m
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> icecreamParlor(int m, List<Integer> arr) {
+    // Write your code here
+        Map<Integer , Integer > map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+        for ( int i=0; i<arr.size(); i++){
+            int y = m - arr.get(i) ;
+            Integer finder = map.get(y); 
+            if( finder != null ){
+                ans.add(finder + 1);
+                ans.add( i + 1);
+                break ; 
+            }
+            map.put( arr.get(i) , i );
+            
+        }
+        return ans ; 
+    }   
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrTemp[i]);
+                arr.add(arrItem);
+            }
+
+            List<Integer> result = Result.icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.size(); i++) {
+                bufferedWriter.write(String.valueOf(result.get(i)));
+
+                if (i != result.size() - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ Algorithms/Search/Missing_Number.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'missingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER_ARRAY brr
+     */
+
+    public static List<Integer> missingNumbers(List<Integer> arr, List<Integer> brr) {
+        Map<Integer , Integer> map = new HashMap<>();
+        for( int i : arr ){
+            map.put( i , map.getOrDefault( i , 0 ) + 1);
+        }
+        for( int i : brr ){
+            if ( freq == 0 ){
+                map.remove(i);
+            }else{
+                map.put( i , map.getOrDefault( i , 0 ) + 1);
+            }
+        }
+        List<Integer> missing = new ArrayList<>(map.keySet());
+        return missing;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] brrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> brr = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            int brrItem = Integer.parseInt(brrTemp[i]);
+            brr.add(brrItem);
+        }
+
+        List<Integer> result = Result.missingNumbers(arr, brr);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
